a8af47b0ae5a8012c603d9b67b1252e87320bbab
Rafaela Silverio <rafaelapetelin@gmail.com>
2020-11-29 18:04:55 -0300

    mudanças no upload

--- upload.php
+++ upload.php
@@ -2,18 +2,8 @@
 
 include_once('conexao.php');
 
-function randString($size) {
-    //String com valor possiveis do resultado, os caracteres pode ser adicionado ou retirados conforme sua necessidade
-    $basic = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    $return = "";
-    for ($count = 0; $size > $count; $count++) {
-        //Gera um caracter aleatorio
-        $return .= $basic[rand(0, strlen($basic) - 1)];
-    }
-    return $return;
-}
 
-$nome_final = randString(20) . ".png";
+$nome_final = ".png";
 
 if (substr($_FILES['arquivo']['name'], -3) == "png") {
     $dir = './arquivos/';
