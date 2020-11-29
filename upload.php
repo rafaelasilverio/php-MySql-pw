9a88721ccbee4126113d1900b5d0128c764a00f3
Rafaela Silverio <rafaelapetelin@gmail.com>
2020-11-29 19:43:12 -0300

    agora vai

--- upload.php
+++ upload.php
@@ -10,29 +10,29 @@ if($tipo == "png"){
     $nome = $_GET["name"];
     $nome_final = $nome . ".png";
 
-if (substr($_FILES['arquivo']['name'], -3) == "png") {
-    $dir = './arquivos/';
-    $tmpName = $_FILES['arquivo']['tmp_name'];
-    $name = $_FILES['arquivo']['name'];
-    // move_uploaded_file
+  if (substr($_FILES['arquivo']['name'], -3) == "png") {
+      $dir = './arquivos/';
+      $tmpName = $_FILES['arquivo']['tmp_name'];
+      $name = $_FILES['arquivo']['name'];
+      // move_uploaded_file
     if (move_uploaded_file($tmpName, $dir . $nome_final)) {
          $sqlstring = "insert into imagens (id, arquivo) values (null, '$nome_final')";
         mysqli_query($conexao, $sqlstring);
         header('Location: index.php');
-  } 
-}
+    } 
+  }
 }
   
 if($tipo == "jpg"){
  
-$nome = $_GET["name"];
-$nome_final = $nome . ".jpg";
+    $nome = $_GET["name"];
+    $nome_final = $nome . ".jpg";
 
-if (substr($_FILES['arquivo']['name'], -3) == "png") {
-    $dir = './arquivos/';
-    $tmpName = $_FILES['arquivo']['tmp_name'];
-    $name = $_FILES['arquivo']['name'];
-    // move_uploaded_file
+  if (substr($_FILES['arquivo']['name'], -3) == "jpg") {
+      $dir = './arquivos/';
+      $tmpName = $_FILES['arquivo']['tmp_name'];
+      $name = $_FILES['arquivo']['name'];
+      // move_uploaded_file
     if (move_uploaded_file($tmpName, $dir . $nome_final)) {
          $sqlstring = "insert into imagens (id, arquivo) values (null, '$nome_final')";
         mysqli_query($conexao, $sqlstring);
@@ -41,7 +41,7 @@ if (substr($_FILES['arquivo']['name'], -3) == "png") {
         echo "Erro ao gravar o arquivo";
     }
 } else {
-    echo "Não é documento png";
+    echo "Não é documento jpg";
 }
 
 }else {
