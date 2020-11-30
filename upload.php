5963f7e44335e6c3d27f9dac642f143a194a364a
Rafaela Silverio <rafaelapetelin@gmail.com>
2020-11-30 11:02:34 -0300

    alteração name

--- upload.php
+++ upload.php
@@ -28,10 +28,10 @@ if($tipo == "jpg"){
     $nome = $_POST["nome"];
     $nome_final = $nome . ".jpg";
 
-    if (substr($_FILES['arquivo']['nome'], -3) == "jpg") {
+    if (substr($_FILES['arquivo']['name'], -3) == "jpg") {
         $dir = './arquivos/';
         $tmpName = $_FILES['arquivo']['tmp_name'];
-        $name = $_FILES['arquivo']['nome'];
+        $name = $_FILES['arquivo']['name'];
         // move_uploaded_file
         if (move_uploaded_file($tmpName, $dir . $nome_final)) {
              $sqlstring = "insert into imagens (id, arquivo, nome) values (null, '$nome_final')";
